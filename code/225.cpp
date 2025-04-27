139f544af7f608f683129cc7cce44b7e9b7f6164
tkgaolol <tkgao@connect.ust.hk>
2025-04-27 13:07:32 +0800

    20250426

---
+++ code/225.cpp
@@ -0,0 +1,51 @@
+class MyStack {
+public:
+    queue<int> que;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        que.push(x);
+    }
+    
+    int pop() {
+        int size = que.size();
+        size--;
+        while(size--)
+        {
+            que.push(que.front());
+            que.pop();
+        }
+        int result = que.front();
+        que.pop();
+        return result;
+    }
+    
+    int top() {
+        int size = que.size();
+        size--;
+        while(size--)
+        {
+            que.push(que.front());
+            que.pop();
+        }
+        int result = que.front();
+        que.push(que.front());
+        que.pop();
+        return result;
+    }
+    
+    bool empty() {
+        return que.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
