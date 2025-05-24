25964e8242e82c66c313e533f8e83fbb9c75c90a
tkgaolol <tkgao@connect.ust.hk>
2025-05-24 12:50:24 +0800

    20250524

--- code/513.cpp
+++ code/513.cpp
@@ -31,3 +31,47 @@ public:
         return result;
     }
 };
+
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth = INT_MIN;
+    int result;
+    void traversal(TreeNode* node, int depth)
+    {
+        if(node->left == NULL && node->right == NULL)
+        {
+            if (depth > maxDepth) 
+            {
+                maxDepth = depth;
+                result = node->val;
+            }
+        }
+        if(node->left != NULL) traversal(node->left, depth+1);
+        if(node->right != NULL) traversal(node->right, depth+1);
+    }
+
+    int findBottomLeftValue(TreeNode* root) {
+        traversal(root, 0);
+        return result;
+    }
+};
