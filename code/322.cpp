86383c937eb964e7abd80c60676be82bc066c2af
tkgaolol <tkgao@connect.ust.hk>
2025-08-29 10:19:25 +0800

    20250829

---
+++ code/322.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp (amount+1, INT_MAX);
+        dp[0] = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = coins[i]; j<= amount; j++)
+            {
+                if(dp[j - coins[i]] != INT_MAX)
+                    dp[j] = min(dp[j-coins[i]] + 1, dp[j]);
+            }
+        }
+        if(dp[amount] == INT_MAX) return -1;
+        return dp[amount];
+
+    }
+};
