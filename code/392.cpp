560b56a0c8ff51cd02f214a071a70332035c2434
tkgaolol <tkgao@connect.ust.hk>
2025-09-17 09:50:18 +0800

    20250917

---
+++ code/392.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        vector<vector<int>> dp (t.size()+1, vector<int> (s.size()+1,0));
+        for(int i = 1; i<=t.size(); i++)
+        {
+            for(int j = 1; j<=s.size();j++)
+            {
+                if(t[i-1]==s[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+        return dp[t.size()][s.size()]==s.size();
+    }
+};
