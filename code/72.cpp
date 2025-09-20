4a017a2d408d22b3b99fcc16bdd1ac8f7ce6a5d2
tkgaolol <tkgao@connect.ust.hk>
2025-09-20 13:39:47 +0800

    20250920

---
+++ code/72.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int a = word1.size();
+        int b = word2.size();
+        vector<vector<int>> dp (a+1, vector<int> (b+1, 0));
+        for(int i = 0; i <= a; i++) dp[i][0] = i;
+        for(int i = 0; i <= b; i++) dp[0][i] = i;
+        for(int i = 1; i <= a; i++)
+        {
+            for(int j = 1; j <= b; j++)
+            {
+                if(word1[i-1]!=word2[j-1])
+                {
+                    dp[i][j] = min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]})+1;
+                }
+                else
+                    dp[i][j] = dp[i-1][j-1];
+            }
+        }
+        return dp[a][b];
+    }
+};
